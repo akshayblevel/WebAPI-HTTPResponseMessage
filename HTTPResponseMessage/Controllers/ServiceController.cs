f4b2ceb8dcffd593d6715a30ab8a306b94831c81
akshayblevel <akshayblevel@hotmail.com>
2018-10-13 00:15:46 +0530

    First Commit

---
+++ HTTPResponseMessage/Controllers/ServiceController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTPResponseMessage.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        static List<string> serviceData = LoadService();
+
+        public static List<string> LoadService()
+        {
+            return new List<string>() { "Mobile Recharge", "Bill Payment" };
+        }
+        // GET: api/Service
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse<IEnumerable<string>>(HttpStatusCode.OK,serviceData);
+        }
+
+        // GET: api/Service/5
+        public HttpResponseMessage Get(int id)
+        {
+            if (serviceData.Count > id)
+                return Request.CreateResponse<string>(HttpStatusCode.OK, serviceData[id]);
+            else
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Item Not Found");
+        }
+
+        // POST: api/Service
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            serviceData.Add(value);
+            return Request.CreateResponse(HttpStatusCode.Created, "Item Added Successfully");
+        }
+
+        // PUT: api/Service/5
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+            serviceData[id] = value;
+            return Request.CreateResponse(HttpStatusCode.OK, "Item Updated Successfully");
+        }
+
+        // DELETE: api/Service/5
+        public HttpResponseMessage Delete(int id)
+        {
+            serviceData.RemoveAt(id);
+            return Request.CreateResponse(HttpStatusCode.OK, "Item Deleted Successfully");
+        }
+    }
+}
---
+++ HTTPResponseMessage/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HTTPResponseMessage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HTTPResponseMessage")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("554d6d4a-44d4-4dad-8c58-d90bd7f742d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
